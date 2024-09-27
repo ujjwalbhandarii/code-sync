5f40a2c70203e74eec8257f4f50db21c94f84262
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-28 00:14:26 +0545

    server init

---
+++ apps/server/src/app.ts
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express from "express";
+import { config } from "dotenv";
+
+const PORT = 9999;
+const app = express();
+
+config();
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: "*",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+function bootstrap() {
+  app.get("/", (req, res) => {
+    res.send("Server running...");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`);
+  });
+}
+
+bootstrap();
