2a433d3a58e18c6fefe09cf39370463fd99c2fdd
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-28 23:58:09 +0545

    added nu-monaco-editor

--- apps/web/src/app/pages/home/home.component.ts
+++ apps/web/src/app/pages/home/home.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { NuMonacoEditorModule } from '@ng-util/monaco-editor';
 
 @Component({
-  selector: 'app-home',
   standalone: true,
-  imports: [],
+  selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: ``
+  imports: [NuMonacoEditorModule, CommonModule, FormsModule],
 })
 export class HomeComponent {
+  value: string = 'const a = 1;';
 
+  editorOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
+    theme: 'vs-dark',
+    language: 'typescript',
+    fontSize: 16,
+    wordWrap: 'on',
+    minimap: { enabled: true },
+    tabSize: 4,
+    padding: {
+      top: 20,
+      bottom: 20,
+    },
+  };
+
+  onValueChange(newValue: string) {
+    this.value = newValue;
+  }
 }
