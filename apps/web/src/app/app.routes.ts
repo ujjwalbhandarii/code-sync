b90230c0ff3aad90d28446cc7f13ade0813849b6
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-04-08 18:52:40 +0545

    refactor: routes config

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
+
+import { ROUTES } from '@/constant/route.constant';
 import { HomeComponent } from './home/home.component';
 import { EditorComponent } from './editor/editor.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
     title: 'Homepage',
+    component: HomeComponent,
+    path: ROUTES.BASEPATH.HOME,
   },
   {
-    path: 'editor',
-    component: EditorComponent,
     title: 'Editor',
+    component: EditorComponent,
+    path: ROUTES.BASEPATH.EDITOR,
   },
 ];
---
+++ apps/web/src/constant/route.constant.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  BASEPATH: {
+    HOME: '',
+    EDITOR: 'editor',
+  },
+};
